b69673140e849d43df6644e4eb13ff955c4585b1
nassim <nassim.bouhadouf@gmail.com>
2021-04-17 00:43:54 +0100

    v_1

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 //app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
-app.use(express.static('${__dirname}/front-end/dist/'));
+app.use(express.static('${__dirname}/dist/animator'));
 
 app.get('*', (req, res) => {
-    res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname+`/dist/animator/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 /*app.get('/*', (req,res,next) => {
