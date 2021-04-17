886967276f5a8b759cbefed1ce6685788213010c
nassim <nassim.bouhadouf@gmail.com>
2021-04-17 01:16:04 +0100

    v_1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 //app.use(express.static(__dirname + '/dist/<FOLDER_NAME>'));
-app.use(express.static('${__dirname}/dist/animator'));
+app.use(express.static(__dirname+'/dist/animator'));
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname+`/dist/animator/index.html`); // load the single view file (angular will handle the page changes on the front-end)
